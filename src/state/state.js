2feeeb805aa442d5bfced413cff739c47297b7b1
marvall <yariknarik.xxx@gmail.com>
2021-01-07 14:53:10 +0200

    remake flux, add style header

--- src/state/state.js
+++ src/state/state.js
@@ -1,7 +1,16 @@
 const photo = require("./galleryitem.json");
 
+let renderEntriesTree = () => {
+  console.log(
+    "Function was changhe STATE. if you see this messages something was wrong "
+  );
+};
+
 const state = {
   gallery: photo,
 };
 
+export const subscribe = (observer) => {
+  renderEntriesTree = observer;
+};
 export default state;
